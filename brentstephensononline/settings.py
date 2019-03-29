885b24579349f63bb5d31587dab8bd336b123d5e
RBStephenson <brentstephenson@hotmail.com>
2019-03-29 13:04:20 -0400

    Continuing to learn and flesh out portions of the site.

--- brentstephensononline/settings.py
+++ brentstephensononline/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'blog',
     'taggit',
+    'material.theme.bluegrey',
+    'material',
+    'material_blog',
 ]
 
 MIDDLEWARE = [
